f82af37536d0864745cb4a824ac2da3a2722f2ff
gitoshh <karani.gitonga.godwin@gmail.com>
2019-04-28 13:46:57 +0300

    Add user creation functionality

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @var Request
+     */
+    public $request;
+
+    /**
+     * Controller constructor.
+     *
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Returns a request payload value given the key.
+     *
+     * @param $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->request->get($key);
+    }
 }
